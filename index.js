9f5925f8c3547f4b32b852b63ec870fa7ae63501
Tema <public@mail.ru>
2024-04-05 09:54:07 +0300

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '6403155092:AAHGKwZGQXYqZ23F-V5b2NIaomcZT00D8B4';
+const token = '6403155092: AAHGKwZGQXYqZ23F-V5b2NIaomcZT00D8B4';
 const webAppUrl = 'https://bejewelled-mooncake-c33499.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
@@ -15,16 +15,23 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-
-
-        await bot.sendMessage(chatId, 'Заходи в приложение по кнопке ниже', {
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Слушать музыку', web_app: {url: webAppUrl}}]
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
                 ]
             }
         })
 
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
 
     if(msg?.web_app_data?.data) {
         try {
