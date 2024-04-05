48bf9487e60c83fe51286326d125d78e3870d199
Tema <public@mail.ru>
2024-04-05 10:18:29 +0300

    init

--- index.js
+++ index.js
@@ -67,6 +67,4 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
-const PORT = 8000;
-
-app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+app.listen(() => console.log('server started'))
